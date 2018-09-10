d104120b1a0b0a6a2ca63fa3eaa850a542751340
TATIBOUET <darkana.polofowl@gmail.com>
2018-09-10 17:49:48 +0200

    HTML home

--- index.php
+++ index.php
@@ -1,5 +1,63 @@
-<html>
-<body>
-    <h1>TEST</h1>
-</body>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Evender</title>
+</head>
+    <body>
+        <header>
+            <div class="btn-add_event">
+                <img src="img/add-event.svg" alt="">
+            </div>
+            <div class="logo">
+                <h1>
+                    <img src="img/logo.svg" alt="Evender">
+                </h1>
+            </div>
+            <div class="btn-login">
+                <a href="">
+                    <img src="img/disconnection.svg" alt="disconnection">
+                </a>
+            </div>
+        </header>
+        <main>
+            <section class="event">
+                <img src="img/events/bowling.jpg" alt="event">
+                <div class="infos">
+                    <p>Bowling, 12/09/2018, Nantes</p>
+                </div>
+                <div class="nb-persons">
+                    <img src="img/nb-person.svg" alt="">
+                    <p>8</p>
+                </div>
+                <div class="admin">
+                    <p>Alexis</p>
+                    <div class="medal">
+                        <img src="img/medals/medal-wood.png" alt="medal-wood" class="medal" >
+                        <img src="img/medals/medal-ambiance.png" alt="medal-ambiance" class="medal" >
+                        <img src="img/medals/medal-price.png" alt="medal-price" class="medal" >
+                        <img src="img/medals/medal-quality.png" alt="medal-quality" class="medal" >
+                    </div>
+                </div>
+                <div class="description">
+                    <p>Quis enim aut eum diligat quem metuat, aut eum a quo se metui putet? Coluntur tamen simulatione dumtaxat ad tempus.</p>
+                </div>
+            </section>
+            <section>
+                <button class="validate">
+                    <img src="img/validate.svg" alt="validate">
+                </button>
+                <button class="eject">
+                    <img src="img/eject.svg" alt="eject">
+                </button>
+            </section>
+        </main>
+        <footer>
+            <div>
+                <a href="">Mes évènements</a>  
+            </div>
+        </footer>
+    </body>
 </html>
