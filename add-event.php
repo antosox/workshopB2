bd40997ab4059bcb59809e7c9f508b89a4feccd7
TATIBOUET <darkana.polofowl@gmail.com>
2018-09-10 18:33:16 +0200

    HTML add event

---
+++ add-event.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <section class="add-event">
+        <h1>Ajouter un évènement</h1>
+        <form action="">
+            <img src="img/picture.svg" alt="">
+            <input type="file" name="add_picture" id="add_picture">
+            <input type="text" name="title_event" id="title_event" placeholder="Titre de l'évènement">
+            <input type="text" name="admin_event" id="admin_event">
+            <img src="img/clock.png" alt="clock">
+            <input type="datetime" name="date_event" id="date" placeholder="Date et heure">
+            <img src="img/place.png" alt="place">
+            <input type="text" name="address_event" id="address_event">
+            <input type="submit" value="Valider">
+        </form>
+    </section>
+</body>
+</html>
+
+
+
+
