235ee3825d7c0de7a38de7cddc687d0a6df40c7f
TATIBOUET <darkana.polofowl@gmail.com>
2018-09-11 10:13:18 +0200

    header index CSS

--- index.php
+++ index.php
@@ -14,39 +14,38 @@
 </head>
     <body>
         <header>
-            <div class="btn-add_event">
+            <button class="btn-add_event btn-flat">
                 <img src="img/add-event.svg" alt="">
-            </div>
+            </button>
+            <button class="btn-off btn-flat">
+                <img src="img/disconnection.svg" alt="disconnection">
+            </button>
             <div class="logo">
                 <h1>
                     <img src="img/logo.svg" alt="Evender">
                 </h1>
             </div>
-            <div class="btn-off">
-                <a href="#">
-                    <img src="img/disconnection.svg" alt="disconnection">
-                </a>
-            </div>
         </header>
         <main>
             <section class="event">
-                <img src="img/events/bowling.jpg" alt="event">
+                <img src="img/events/bowling.jpg" alt="event" class="illustration">
                 <div class="infos">
                     <h2>Bowling</h2>
-                    <p>12/09/2018, Nantes</p>
+                    <p>12/09/2018</p>
+                    <p>Nantes</p>
                 </div>
                 <div class="nb-persons">
                     <img src="img/nb-person.svg" alt="">
                     <p>8</p>
                 </div>
-                <div class="admin">
+                <div class="admin-organizer">
                     <p>Alexis</p>
-                    <div class="medal">
+                </div>
+                <div class="medal-organizer">
                         <img src="img/medals/medal-wood.png" alt="medal-wood" class="medal" >
                         <img src="img/medals/medal-ambiance.png" alt="medal-ambiance" class="medal" >
                         <img src="img/medals/medal-price.png" alt="medal-price" class="medal" >
                         <img src="img/medals/medal-quality.png" alt="medal-quality" class="medal" >
-                    </div>
                 </div>
                 <div class="description">
                     <p>Quis enim aut eum diligat quem metuat, aut eum a quo se metui putet? Coluntur tamen simulatione dumtaxat ad tempus.</p>
