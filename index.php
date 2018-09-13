be054701fb502ed44c441429e87b5904293ca5af
TATIBOUET <darkana.polofowl@gmail.com>
2018-09-14 01:23:26 +0200

    delete infinity loop

--- index.php
+++ index.php
@@ -188,25 +188,15 @@ function submit() {
                     user: user};
             ws.send(JSON.stringify(data));         
         }
-while(1<2){
-    $(function(){
-    if ($("#test").hasClass('tinder_love')){
-            console.log("1");
-            submit();
-    } else {
-        console.log("0");
-    }
-    });
-}
-
-        // var elm = document.getElementById('test');
         
-        // if(elm.className === 'tinder_love'){
-        //     console.log('1');
-        //     submit();
-        // } else {
-        //     console.log('0');
-        // }
+    $(function(){
+        if ($("#test").hasClass('tinder_love')){
+                console.log("1");
+                submit();
+        } else {
+            console.log("0");
+        }
+    });
 
         </script>
 </html>
